7b2e51d6112fea3d2c5d707e65998b0c2d1918e7
Juan Carvajal <jdc.diaz@yavirac.edu.ec>
2019-02-07 10:17:52 -0500

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './Componentes/home/home.component';
+import { LoginComponent } from './Componentes/login/login.component';
+import { RegisterComponent } from './Componentes/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
