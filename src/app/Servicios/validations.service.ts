8514b83e8b406d08ecfcaa84e431847d45dabeff
Juan Carvajal <jdc.diaz@yavirac.edu.ec>
2019-02-07 17:47:17 -0500

    registro

--- src/app/Servicios/validations.service.ts
+++ src/app/Servicios/validations.service.ts
@@ -11,10 +11,6 @@ export class ValidationsService {
 
   login(email: String, pasword: String): Promise<any> {
     const data = {email: email, pasword: pasword};
-<<<<<<< HEAD
-=======
-    JSON.stringify(console.log(data));
->>>>>>> d0a6c263891299989148eac35ac2b02e56329c52
     return this.http.post(environment.api + 'login', JSON.stringify(data)).toPromise()
     .then( r =>
       r.json()
@@ -23,9 +19,9 @@ export class ValidationsService {
     });
   }
 
-  register(name: String, email: String): Promise<any> {
-    const data = {name: name, email: email};
-    return this.http.post(environment.api + 'register', JSON.stringify(data)).toPromise()
+  users(nombre: String, apellido: String, usuario:String, carrera:String, email: String, pasword: String): Promise<any> {
+    const data = {nombre: nombre, apellido: apellido, usuario: usuario, carrera: carrera, email: email, pasword: pasword};
+    return this.http.post(environment.api + 'users', JSON.stringify(data)).toPromise()
     .then( r =>
       r.json()
     ).catch( error => {
--- src/app/componentes/register/register.component.ts
+++ src/app/componentes/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ValidationsService } from './../../Servicios/validations.service';
+import { Router } from '@angular/router';
+import swal from 'sweetalert';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  nombre: String = "";
+  apellido: String = "";
+  usuario: String = "";
+  carrera: String = "";
+  email: String = "";
+  pasword: String ="";
+  busy: Promise<any>;
+  esperando: Boolean;
+
+  constructor(private router: Router, private validationsDataServise:ValidationsService ) { }
 
   ngOnInit() {
+    this.nombre='';
+    this.apellido='';
+    this.usuario='';
+    this.carrera='';
+    this.email='';
+    this.pasword='';
+    this.esperando= false;
+  }
+
+  users() {
+    if ( !this.esperando ) {
+      this.esperando = true;
+      this.busy = this.validationsDataServise.users(this.nombre, this.apellido, this.usuario, this.carrera, this.email, this.pasword).then( r => {
+        this.esperando = false;
+        swal({
+          title: 'Te damos la bienvenida',
+          text: 'Ahora podras ingresar',
+          icon: 'success',
+        })
+        .then( response => {
+          this.nombre='';
+          this.apellido='';
+          this.usuario='';
+          this.carrera='';
+          this.email='';
+          this.pasword='';
+          this.router.navigate(['/login']);
+        });
+      }).catch( e => {
+        this.esperando = false;
+        console.log(e);
+      });
+    }
   }
 
 }
