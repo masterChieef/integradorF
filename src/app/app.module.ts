21dcaafbd744c9c2eb56e845d6c78b23005eb102
Juan Carvajal <jdc.diaz@yavirac.edu.ec>
2019-02-07 10:03:36 -0500

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Componentes/home/home.component';
+import { NavbarComponent } from './Componentes/navbar/navbar.component';
+import { LoginComponent } from './Componentes/login/login.component';
+import { RegisterComponent } from './Componentes/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
