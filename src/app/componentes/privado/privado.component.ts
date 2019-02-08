7d639953dfd477c5188bfb7732474cbe241719b6
Juan Carvajal <jdc.diaz@yavirac.edu.ec>
2019-02-08 08:27:32 -0500

    privado1

--- src/app/componentes/privado/privado.component.ts
+++ src/app/componentes/privado/privado.component.ts
@@ -12,4 +12,50 @@ export class PrivadoComponent implements OnInit {
   ngOnInit() {
   }
 
+  msg:string = '';
+
+  employees = [
+    {'name': 'Esto es una prueba de SocializeApp', position: 'manager', email:''},
+    {'name': 'Hola Mundo', position: ' ', email:''},
+    {'name': ' ', position: ' ', email:''}
+  ];
+
+  model:any = {};
+  model2:any = {};
+  hideUpdate:boolean = true;
+
+  addEmployee():void{
+    this.employees.push(this.model);
+    this.msg = 'campo agregado';
+  }
+
+  deleteEmployee(i):void {
+    var answer = confirm('Estas seguro querer eliminarlo?');
+    if(answer) {
+      this.employees.splice(i, 1);
+      this.msg = 'campo eliminado';
+    }
+  }
+
+  myValue;
+  editEmployee(i):void {
+    this.hideUpdate = false;
+    this.model2.name = this.employees[i].name; 
+    this.myValue = i;
+  }
+
+  updateEmployee():void {
+    let i = this.myValue;
+    this.hideUpdate = true;
+    for(let j = 0; j < this.employees.length; j++){
+      if(i == j) {
+        this.employees[i] = this.model2;
+        this.msg = 'campo actualizado';
+        this.model2 = {};
+      }
+    }
+  }
+  closeAlert():void {
+    this.msg = '';
+  }
 }
---
+++ src/app/modelos/user.ts
@@ -0,0 +1,12 @@
+export class User{
+    id: number;
+    nombre: String;
+    apellido: String;
+    usuario: String;
+    carrera: String;
+    email: String;
+    created_at: Date;
+    updated_at: Date;
+    detalle: String;
+    user_id: number;k
+}
